aafd286ed8ac76972dc9afb5ef025a836567715f
henry perez <hperezcbba@gmail.com>
2018-10-11 16:12:40 -0400

    fsdfsdf

--- application/views/pages_public/home.php
+++ application/views/pages_public/home.php
@@ -32,7 +32,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#" >Tu Agenda</a>
+				<a class="navbar-brand" href="#" >Tu Guia Virtual</a>
 			</div>
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
